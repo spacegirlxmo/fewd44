371a390102006258ff363999cd0248981f55381b
Monique Solanki <monique.aruna@gmail.com>
2016-05-06 17:21:29 -0700

    added js from console code-along

---
+++ travel-news/js/travel.js
@@ -0,0 +1,7 @@
+var child = document.createElement("li");
+
+var sponsoredArticles = document.querySelector(".sponsored-articles");
+
+parent.appendChild(child);
+
+child.textContent = "Trump wins 37 states! New USA PREZ!";
