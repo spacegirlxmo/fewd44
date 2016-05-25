6a5c8f8d478c7232563a48539300d4082eef1bda
Monique Solanki <monique.aruna@gmail.com>
2016-05-24 21:26:15 -0700

    added js functions

--- compare/js/compare.js
+++ compare/js/compare.js
@@ -6,9 +6,27 @@ var sign = document.querySelector(".sign");
 var compare = document.querySelector(".compare");
 
 // Events
-compare.addEventListener('click', compareNumbers);
+compare.addEventListener("click", compareNumbers);
+
+// Listen for the compare button to be clicked
+
+// Compare the first and second number
 
 // Event Handlers
+// -------------------------
+// Update the page - Change the sign
 function compareNumbers () {
-  sign.textContent = "=";
+
+  var firstValue = firstNumber.value;
+  firstValue = parseInt(firstValue);
+
+  var secondValue = secondNumber.value;
+  secondValue = parseInt(secondValue);
+
+  // JS Conditional (if, else if, else)
+  if (firstValue > secondValue) {
+    sign.textContent = ">";
+  } else if (firstValue < secondValue) {
+    sign.textContent = "<";
+  }
 }
