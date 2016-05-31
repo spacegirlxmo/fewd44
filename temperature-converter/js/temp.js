47075b098d21f492a64254830eadf5fd3302b79b
Monique Solanki <monique.aruna@gmail.com>
2016-05-30 23:17:28 -0700

    added styling and js logic and an image png

--- temperature-converter/js/temp.js
+++ temperature-converter/js/temp.js
@@ -1,11 +1,12 @@
 // Markup
 
 var fahrenheit = document.querySelector(".fahrenheit");
-var convert = document.querySelector(".convert");
+var celsius = document.querySelector(".celsius");
 
 // Events
 
-convert.addEventListener("click", fahrenheitToCelsius);
+fahrenheit.addEventListener("input", fahrenheitToCelsius);
+celsius.addEventListener("input", celsiusToFahrenheit);
 
 
 //Event Handlers
@@ -14,11 +15,28 @@ function fahrenheitToCelsius() {
   console.log("fn fahrenheitToCelsius");
 
   //get data from page
-  var x = parseInt(fahrenheit.value);
-  console.log("x:", x)
+  var fahrenheitInput = parseInt(fahrenheit.value);
+  console.log("Temp:", fahrenheitInput);
 
   //do some work/processing
-  var z = 3 * x;
+  var celsiusConvert = (fahrenheitInput - 32) * 5 / 9;
+  celsiusConvert = celsiusConvert.toPrecision(3);
+
   //update the page
-  fahrenheit.value = z;
+  celsius.value = celsiusConvert;
+}
+
+function celsiusToFahrenheit() {
+  console.log("fn celsiusToFahrenheit");
+
+  //get data from page
+  var celsiusInput = parseInt(celsius.value);
+  console.log("Temp:", celsiusInput);
+
+  //do some work/processing
+  var fahrenheitConvert = (celsiusInput * 9) /  5 + 32;
+  fahrenheitConvert = fahrenheitConvert.toPrecision(3);
+
+  //update the page
+  fahrenheit.value = fahrenheitConvert;
 }
