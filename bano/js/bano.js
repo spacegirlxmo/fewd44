ef776721175032c6d86dcad06c8e81939e0a8a85
Monique Solanki <monique.aruna@gmail.com>
2016-07-04 23:47:10 -0700

    added locations

--- bano/js/bano.js
+++ bano/js/bano.js
@@ -18,8 +18,31 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic3BhY2VnaXJseG1vIiwiYSI6ImNpcHFiM28wYTA1b21mb
 var map = new mapboxgl.Map({
 container: 'map', // container id
 style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
-center: [-89, 89], // starting position
+center: [-121.8863, 37.3382], // starting position
 zoom: 14 // starting zoom
 });
 
 getLocation();
+
+var locations = document.querySelector(".locations");
+
+
+
+function updateLocations() {
+  bathrooms.forEach(createBathroom);
+}
+
+function createBathroom(bathrooms) {
+  var li = document.createElement("li");
+  var p = document.createElement("p");
+  var p2 = document.createElement("p");
+
+  p.textContent = bathrooms.name;
+  p2.textContent = bathrooms.street_address;
+
+  li.appendChild(p);
+  li.appendChild(p2);
+  locations.appendChild(li);
+}
+
+updateLocations();
---
+++ bano/js/content.js
@@ -0,0 +1,36 @@
+var locations = document.querySelector(".locations");
+
+var leesDeli = {
+  "name": "Lee's Deli",
+  "street_address": "235 Montgomery St.",
+  "long": -122.402609,
+  "lat": 37.791537
+}
+
+var bodyAndSole = {
+  "name": "Body and Sole",
+  "street_address": "41 Sutter St., Suite 617",
+  "long": -122.401816,
+  "lat": 37.789067
+}
+
+var starsBarAndDining = {
+  "name": "Stars Bar and Dining",
+  "street_address": "555 Golden Gate Ave.",
+  "long": -122.419505,
+  "lat": 37.781042
+}
+
+var starbucks = {
+  "name": "Starbucks",
+  "street_address": "44 Montgomery St.",
+  "long": -122.401736,
+  "lat": 37.789778
+}
+
+var bathrooms = [
+  leesDeli,
+  bodyAndSole,
+  starsBarAndDining,
+  starbucks
+];
